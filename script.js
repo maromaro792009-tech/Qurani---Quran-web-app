2c20096d3a496f1c698d68cf157e17ecc27d6ed5
marwan <maromaro792009@gmail.com>
2026-07-11 16:07:01 +0300

    Implement search feature for surahs
    Added search functionality to filter surahs by name.

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const test = document.querySelector(".test")
 const cardsArea = document.querySelector(".cards-area")
+const searchBtn = document.querySelector(".search-btn")
+const searchArea = document.querySelector(".search-area")
+const searchInput = document.querySelector(".search")
+const exitBtn = document.querySelector(".exit-btn")
+searchBtn.addEventListener("click", () => {
+    searchArea.classList.toggle("appear")
+    setTimeout(() => {
+        searchInput.click()
+        searchInput.focus()
+    }, 100)
+})
 
+exitBtn.addEventListener("click", () => {
+    searchArea.classList.remove("appear")
+})
 
-async function startApp() {
-    const res = await fetch("https://ummahapi.com/api/quran/surahs?reciter=alafasy")
-    const info = await res.json()
-    info.data.surahs.forEach(one => {
+document.addEventListener("click", (e) => {
+    if (!e.target.closest(".search-area") && !e.target.closest(".search-btn")) {
+        searchArea.classList.remove("appear")
+    }
+})
+function rerender(one) {
     const surah = document.createElement("article")
     const btn = document.createElement("button")
     const names = document.createElement("div")
@@ -31,11 +47,26 @@ async function startApp() {
         }
     })
     audio.addEventListener("ended", () => {
-        btn.innerHTML = '<svg class="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/></svg>'    
+        btn.innerHTML = '<svg class="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/></svg>'
+        
     })
     names.append(surahName, surahNum)
     surah.append(btn, names)
     cardsArea.append(surah)
+}
+
+async function startApp() {
+    const res = await fetch("https://ummahapi.com/api/quran/surahs?reciter=alafasy")
+    const info = await res.json()
+    info.data.surahs.forEach(one => {
+        rerender(one)
+    })
+    searchInput.addEventListener("input", () => {
+        let value = info.data.surahs.filter(val => val.name_arabic.includes(searchInput.value))
+        cardsArea.innerHTML = ""
+        value.forEach(search => {
+            rerender(search)
+        })
     })
 }
 
